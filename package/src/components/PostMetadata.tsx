2f524966abce56e1a3523eb28ac7a7da2fc7215f
zzzkan <zzz602214076@outlook.com>
2022-11-16 03:31:31 +0900

    Update post metadata

--- package/src/components/PostMetadata.tsx
+++ package/src/components/PostMetadata.tsx
@@ -1,22 +1,40 @@
 import React from "react"
-import { Box, Wrap, WrapItem } from "@chakra-ui/react"
+import { Box, Icon, HStack, Wrap, WrapItem } from "@chakra-ui/react"
+import { BiCalendar } from "@react-icons/all-files/bi/BiCalendar"
+import { BiCalendarEdit } from "@react-icons/all-files/bi/BiCalendarEdit"
+import { BiTime } from "@react-icons/all-files/bi/BiTime"
 
 export type PostMetadataProps = {
   readonly publishedDate: string
   readonly updatedDate: string | null
-  readonly wordCount: number | null
+  readonly timeToReadMinutes: number | null
 }
 
 const PostMetadata: React.FC<PostMetadataProps> = (post) => {
   return (
-    <Wrap spacing={1} fontSize={"sm"}>
+    <Wrap spacingX={1} spacingY={0} fontSize={"sm"}>
       <WrapItem>
-        <Box as="time">{post.publishedDate}</Box>
+        <HStack spacing={0}>
+          <Icon as={BiCalendar} fontSize={"md"} />
+          <Box as="time">{post.publishedDate}</Box>
+        </HStack>
       </WrapItem>
-      <WrapItem>
-        <Box as="time">{post.updatedDate}</Box>
-      </WrapItem>
-      <WrapItem>{post.wordCount}</WrapItem>
+      {post.updatedDate != null && (
+        <WrapItem>
+          <HStack spacing={0}>
+            <Icon as={BiCalendarEdit} fontSize={"md"} />
+            <Box as="time">{post.updatedDate}</Box>
+          </HStack>
+        </WrapItem>
+      )}
+      {post.timeToReadMinutes != null && (
+        <WrapItem>
+          <HStack spacing={0}>
+            <Icon as={BiTime} fontSize={"md"} />
+            <Box>{Math.ceil(post.timeToReadMinutes)}min</Box>
+          </HStack>
+        </WrapItem>
+      )}
     </Wrap>
   )
 }
