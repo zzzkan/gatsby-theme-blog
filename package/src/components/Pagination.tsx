650158e0c36ada36ffdee88a2ee803aaefe0c3e0
zzzkan <zzz602214076@outlook.com>
2022-12-07 00:32:20 +0900

    Improve pagination

--- package/src/components/Pagination.tsx
+++ package/src/components/Pagination.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link as GatsbyLink } from "gatsby"
-import { HStack, Link } from "@chakra-ui/react"
+import { Box, HStack, Link } from "@chakra-ui/react"
 import { useThemeColor } from "../hooks/useThemeColor"
 
 export type PaginationProps = {
@@ -9,53 +9,44 @@ export type PaginationProps = {
   readonly currentPage: number
 }
 
-const width = 3
+const width = 2
 
 const Pagination: React.FC<PaginationProps> = ({
   basePath,
   totalPage,
   currentPage,
 }) => {
-  const { tint, primaryText } = useThemeColor()
+  const { tint } = useThemeColor()
   const pages = [...Array(2 * width + 1)]
     .map((_, i) => i + currentPage - width)
     .filter((page) => page >= 1 && page <= totalPage)
-  const to = (page: number): string => {
+  const path = (page: number): string => {
     if (page === 1) return basePath
     return `${basePath}/${page}`.replace(/\/\/+/g, "/")
   }
   return (
     <HStack spacing={3} fontSize={"xl"}>
       {currentPage !== 1 && (
-        <Link
-          as={GatsbyLink}
-          to={to(currentPage - 1)}
-          color={primaryText}
-          _hover={{ textDecoration: "none" }}
-        >
+        <Link as={GatsbyLink} to={path(currentPage - 1)}>
           {"<"}
         </Link>
       )}
       {pages.map((page) => {
+        if (page === currentPage) {
+          return (
+            <Box key={page} as={"span"} color={tint}>
+              {page}
+            </Box>
+          )
+        }
         return (
-          <Link
-            key={page}
-            as={GatsbyLink}
-            to={to(page)}
-            color={page === currentPage ? tint : primaryText}
-            _hover={{ textDecoration: "none" }}
-          >
+          <Link key={page} as={GatsbyLink} to={path(page)}>
             {page}
           </Link>
         )
       })}
       {currentPage !== totalPage && (
-        <Link
-          as={GatsbyLink}
-          to={to(currentPage + 1)}
-          color={primaryText}
-          _hover={{ textDecoration: "none" }}
-        >
+        <Link as={GatsbyLink} to={path(currentPage + 1)}>
           {">"}
         </Link>
       )}
