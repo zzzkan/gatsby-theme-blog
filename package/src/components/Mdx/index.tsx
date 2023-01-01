56a940c4003e60cc114dc5018a1842d3552acb63
zzzkan <zzz602214076@outlook.com>
2023-01-02 02:15:29 +0900

    Add maringY to paragraph

--- package/src/components/Mdx/index.tsx
+++ package/src/components/Mdx/index.tsx
@@ -20,7 +20,7 @@ import { AnchorHeading } from "./AnchorHeading"
 
 export const Mdx: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const components = {
-    p: (props: object) => <Text {...props} />,
+    p: (props: object) => <Text marginY={3} {...props} />,
     h1: (props: object) => (
       <AnchorHeading
         as={"h1"}
