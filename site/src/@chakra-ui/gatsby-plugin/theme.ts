dbaa797ae700ed0349810a0181d86b3a0138a6b6
zzzkan <zzz602214076@outlook.com>
2023-02-02 00:51:06 +0900

    Rename highlightBackground to highlightCode

--- site/src/@chakra-ui/gatsby-plugin/theme.ts
+++ site/src/@chakra-ui/gatsby-plugin/theme.ts
@@ -39,8 +39,8 @@ const theme = extendTheme({
         default: "RGBA(0, 0, 0, 0.02)",
         _dark: "RGBA(255, 255, 255, 0.02)",
       },
-      highlightBackground: {
-        default: "RGBA(255, 255, 255, 0.03)",
+      highlightCode: {
+        default: "RGBA(255, 255, 255, 0.1)",
       },
     },
   },
